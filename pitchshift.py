a23a785e8f7201a20fb04763e5153dd2e28f320d
Tae-Hwan Sky Kim <k14tk01@kzoo.edu>
2021-04-18 17:50:59 -0500

    pitch shifter - 2

--- pitchshift.py
+++ pitchshift.py
@@ -7,53 +7,50 @@ import pygame
 # sample rate in Hz
 RATE = 44100
 RECORD_SECONDS = 5
-BLOCKLEN = 800
+BLOCKLEN=800
 
-MAXVALUE = 2 ** 15 - 1
+MAXVALUE = 2**15-1
 
-k = 0
+k=0
 
 # Number of blocks to run for
 num_blocks = int(RATE / BLOCKLEN * RECORD_SECONDS)
 
 p = pyaudio.PyAudio()
-wf = wave.open("sample.wav", 'rb')
+# wf = wave.open("sample.wav", 'rb')
 PA_FORMAT = pyaudio.paInt16
+#
+# stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+#                 channels=2,
+#                 rate=44100,
+#                 output=True,
+#                 frames_per_buffer = 800)
 
-stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                channels=2,
-                rate=44100,
-                output=True,
-                frames_per_buffer=800)
-
-# stream = p.open(
-#         format      = PA_FORMAT,
-#         channels    = 1,
-#         rate        = RATE,
-#         input       = True,
-#         output      = True,
-#         frames_per_buffer = 800)
+stream = p.open(
+        format      = PA_FORMAT,
+        channels    = 1,
+        rate        = RATE,
+        input       = True,
+        output      = True,
+        frames_per_buffer = 800)
 
 CONTINUE = True
 KEYPRESS = False
 
 
-def my_function(event):
+def up():
     global CONTINUE
     global KEYPRESS
     global k
-    print('You pressed ' + event.char)
-    if event.char == 'q':
-        print('Good bye')
-        CONTINUE = False
-    if event.char == '+':
-        k += 1
-    if event.char == '-':
-        k -= 1
-    if event.char == '=':
-        k = 0
-    KEYPRESS = True
 
+    k += 1
+
+def down():
+    global CONTINUE
+    global KEYPRESS
+    global k
+
+    k -= 1
 
 def play():
     pygame.init()
@@ -63,13 +60,17 @@ def play():
 
 
 root = tkinter.Tk()
-root.bind("<Key>", my_function)
+# root.bind("<Key>", my_function)
 
-button1 = tkinter.Button(text='play')
-button1.config(command=play)
-print('Press keys for sound.')
-print('Press "q" to quit')
+button1 = tkinter.Button(text='pitch up')
+button1.config(command = up)
+
+button2 = tkinter.Button(text='pitch down')
+button2.config(command = down)
+
 button1.pack()
+button2.pack()
+
 
 # Start loopfgh
 while CONTINUE:
@@ -77,23 +78,23 @@ while CONTINUE:
 
     # Get frames from audio input stream
     # input_bytes = stream.read(BLOCKLEN)       # BLOCKLEN = number of frames read
-    input_bytes = stream.read(BLOCKLEN, exception_on_overflow=False)  # BLOCKLEN = number of frames read
+    input_bytes = stream.read(BLOCKLEN, exception_on_overflow = False)   # BLOCKLEN = number of frames read
     # print(input_bytes)
     # Convert binary data to tuple of numbers
     input_tuple = struct.unpack('h' * BLOCKLEN, input_bytes)
-    data = np.array(input_tuple)
 
     scaled = np.frombuffer(input_bytes, np.int16)
 
     # create a transformer
     tfm = sox.Transformer()
 
+
     # shift the pitch up by 1 semitones
     tfm.pitch(k)
 
-    y_out = 2 * tfm.build_array(input_array=scaled, sample_rate_in=RATE)
+    y_out=2*tfm.build_array(input_array=scaled, sample_rate_in=RATE)
 
-    y = np.clip(y_out.astype(int), -MAXVALUE, MAXVALUE)  # Clipping
+    y = np.clip(y_out.astype(int), -MAXVALUE, MAXVALUE)     # Clipping
 
     output_bytes = struct.pack('h' * BLOCKLEN, *y)
 
